bfb8db03dce80f7c73ff7a18f490cd078b0c2fdc
kol <kol>
2021-11-22 15:51:41 +0300

    добавил список

--- Task1984/Program.cs
+++ Task1984/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task1984
 {
@@ -7,19 +8,34 @@ namespace Task1984
         static void Main(string[] args)
         {
             Line line = new Line(0,0,4,4);
-            Console.WriteLine(line.Draw());
+      
 
             Circle circle = new Circle(2, 2, 6);
-            Console.WriteLine(circle.Draw());
+           
 
             Round round = new Round(2, 2, 6, "Красный");
-            Console.WriteLine(round.Draw());
+        
 
             Ring ring = new Ring(2, 2, 2, 5, "Жёлтый");
-            Console.WriteLine(ring.Draw());
-            Rectangle rectangle = new Rectangle(1,1, 8,1, 1,5, 8,5);
-            Console.WriteLine(rectangle.Draw());
+         
 
+            Rectangle rectangle = new(1,1, 8,1, 1,5, 8,5);
+           
+
+
+            List<Figure> figures = new();
+
+            figures.Add(line);
+            figures.Add(circle);
+            figures.Add(round);
+            figures.Add(ring);
+            figures.Add(rectangle);
+
+            foreach (var figure in figures)
+            {
+                Console.WriteLine(figure.Draw()); 
+
+            }
         }
     }
 }
